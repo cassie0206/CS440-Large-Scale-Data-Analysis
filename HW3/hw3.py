afbcce2c5f7a77919c5f59c5207600ada2c823d0
cassie0206 <cassie610512@gmail.com>
2024-02-18 22:34:14 -0500

    testiing

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -13,32 +13,92 @@ import sys
 # make sure the program prints out your SQL statements correctly. That means the autograder will read you SQL correctly. Running the Python file will not execute your SQL statements (just prints them).
 
 def username():
-	return "YourUsername"
+	return "chan1000"
     
 def query1():
+	#TODO: practice
 	return """
-    SELECT * 
-    FROM Dreams
-    WHERE ending ='happy';
+    select first_name, last_name, salary,
+	lag(salary, 1) over (partition by country = 'Germany' order by salary DESC) - salary as diff
+	from employee
+	where country = 'Germany'
+	order by salary DESC;
 	"""
 
 def query2():
-	return "2"
+	#TODO: practice + window alias 
+	return """
+	select first_name, last_name, salary, country,
+	rank() over (partition by country order by country, salary desc) as rank,
+	ntile(4) over (partition by country order by country, salary desc) as quartile
+	from employee
+	where country = 'Germany' or country = 'United Kingdom'
+	order by country, salary desc
+	"""
 
 def query3():
-	return "3"
+	#TODO: practice!!!
+	return """
+	with temp as(
+	select count(employee_id) as cnt, age, gender
+	from employee
+	where age >= 24 and age <= 30
+	group by gender, age
+	)
+	select age, gender,
+	sum(cnt) over (partition by gender order by age) as employee_num
+	from temp;
+	"""
 	
 def query4():
-	return "4"
+	#TODO: pratice
+	return """
+	select first_name, last_name, age, gender, salary,
+	rank() over (partition by gender order by salary desc) as rank1,
+	rank() over (order by salary desc) as rank2
+	from employee
+	where age=24;
+	"""
 
 def query5():
-	return "5"
+	#TODO: pratice
+	return """
+	with temp as (
+	select department, 
+	avg(salary) over (partition by department) as avg_salary
+	from employee
+	)
+	select distinct temp.department, 
+	avg_salary,
+	case
+	when avg_salary >= 200000 then 'High'
+	when avg_salary < 200000 and avg_salary >= 180000 then 'Medium'
+	else 'Low'
+	end as salary_bracket
+	from employee natural join temp
+	order by department;
+	"""
 
 def query6():
-	return "6"
+	return """
+	
+	"""
 
 def query7():
-	return "7"
+	return """
+	with temp as (
+	select employee_id,
+	case
+		when age < 30 then 'Young'
+		when age >= 30 and age < 50 then 'Middle-aged'
+		else 'Senior'
+	end as age_group
+	from employee
+	)
+	select distinct age_group as 'Age Group', Gender, 
+	count(gender) over (partition by age_group order by gender) as 'Num Employees'
+	from employee natural join temp;
+	"""
 
 def query8():
 	return "8"
